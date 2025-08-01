0d005ef213a38b21349a0d05c08011ebeb0eb4a7
Ralf Ebert <info@ralfebert.de>
2025-08-01 20:57:38 +0200

    cleanup, use proper async logic to perform the sheet screenshot procedure

--- SheetScreenshotTests/AppWindowScreen.swift
+++ SheetScreenshotTests/AppWindowScreen.swift
@@ -1,16 +1,21 @@
-import Testing
 import SnapshotTesting
 import SwiftUI
+import Testing
+
+private var screenshotInProgress = false
 
 /// This class allows rendering multiple screens sequentially, instead of rebuilding a complete View-Tree each time, in order to also test tricky SwiftUI invalidation problems.
 /// This also captures the full UIWindow and applies an ugly workaround to be able to render sheets.
+@MainActor
 class AppWindowScreen<T: View> {
     let controller: UIHostingController<T>
     var dispose: (() -> Void)?
     let view: () -> T
     let window = UIApplication.shared.keyWindow!
 
-    init(@ViewBuilder view: @escaping () -> T, height: Int? = nil) {        
+    init(@ViewBuilder view: @escaping () -> T, height: Int? = nil) {
+        // this is necessary to disable animations for sheets
+        UIView.setAnimationsEnabled(false)
         self.view = view
         self.controller = UIHostingController(rootView: view())
 
@@ -24,7 +29,7 @@ class AppWindowScreen<T: View> {
         controller.view.bounds = CGRect(origin: .zero, size: size)
     }
 
-    func update() {
+    private func update() {
         self.controller.rootView = view()
         self.window.setNeedsLayout()
         self.window.layoutIfNeeded()
@@ -32,18 +37,31 @@ class AppWindowScreen<T: View> {
         CATransaction.flush()
     }
 
-    @discardableResult func renderImage(size: CGSize? = nil) -> UIImage {
-        //self.update()
-        return self.window.asImage(size: size, drawHierarchy: true)
+    private func renderImage(size: CGSize? = nil) -> UIImage {
+        self.window.asImage(size: size, drawHierarchy: true)
+    }
+
+    func updateAndRenderImage() async -> UIImage {
+        // handle re-entrancy, we must not get interrupted while taking a screenshot by another screenshot taking method
+        // as this method is @MainActor, a simple flag as lock-replacement should do the trick
+        while screenshotInProgress {
+            await Task.yield()
+        }
+        screenshotInProgress = true
+        defer { screenshotInProgress = false }
+
+        self.update()
+        // this flushes the main queue, because after a yield, it must get to the back of the main queue to run renderImage
+        await Task.yield()
+        return self.renderImage()
     }
 
     func tearDown() {
-        dispose?()
-        dispose = nil
+        self.dispose?()
+        self.dispose = nil
     }
 
-    deinit {
+    @MainActor deinit {
         self.tearDown()
     }
-
 }
--- SheetScreenshotTests/SheetScreenshotTests.swift
+++ SheetScreenshotTests/SheetScreenshotTests.swift
@@ -1,31 +1,13 @@
+@testable import SheetScreenshot
+import SnapshotTesting
 import SwiftUI
 import Testing
-import SnapshotTesting
-@testable import SheetScreenshot
 
 @Suite(.snapshots(diffTool: .ksdiff))
 struct SheetScreenshotTests {
-
     @Test func exampleScreenshot() async throws {
-        let screen = await Task { @MainActor in
-            // this is necessary to disable animations for sheets
-            UIView.setAnimationsEnabled(false)
+        let image = await AppWindowScreen(view: { ContentView() }).updateAndRenderImage()
 
-            let screen = AppWindowScreen(view: { ContentView() })
-            screen.update()
-            
-            return screen
-        }.value
-        
-        // Use a second @MainActor task to "flush the main queue" - this makes the sheet reliably visible after forcing the update
-        #warning("if multiple tests like this run in parallel we must make sure that we're not interrupted here")
-        let image = await Task { @MainActor in
-            let image = screen.renderImage()
-            screen.tearDown()
-            return image
-        }.value
-        
-        assertSnapshot(of: image, as: .image(perceptualPrecision: 0.99))
+        assertSnapshot(of: image, as: .image(precision: 0.99))
     }
-
 }
